04b83f9336abab07b94e107db5f8619a5c5674c0
Lizard <41157961+lizaard@users.noreply.github.com>
2020-10-30 12:58:02 +0200

    Authentification component add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -11,21 +12,23 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 const routes: Routes = [
 
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, children: [
-    { path: '',  component: RecipeStartComponent },
-    { path: 'new', component: RecipeEditComponent },
-    {
-      path: ':id',
-      component: RecipeDetailComponent,
-      resolve: [RecipeResolverService]
-    },
-    {
-      path: ':id/edit',
-      component: RecipeEditComponent,
-      resolve: [RecipeResolverService]
-    },
+  { path: 'recipes', component: RecipesComponent,
+    children: [
+      { path: '',  component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipeResolverService]
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipeResolverService]
+      },
   ] },
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
