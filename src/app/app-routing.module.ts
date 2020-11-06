f033b6f3e3f821a78dfc6770f10d134641259c63
Lizard <41157961+lizaard@users.noreply.github.com>
2020-11-06 13:34:40 +0200

    End of module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,7 @@ import { AuthComponent } from './auth/auth.component';
 
 
 const routes: Routes = [
-
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-
-
-  { path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,49 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-
-
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
 
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    AuthModule,
+    SharedModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
+  providers: [
+
   ],
   bootstrap: [AppComponent]
 })
