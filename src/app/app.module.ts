015fbd875051b4c5f6cec885bad381917dd0a63a
Lizard <41157961+lizaard@users.noreply.github.com>
2020-11-06 16:38:50 +0200

    Optimize lazy loading code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
-
-import { AuthComponent } from './auth/auth.component';
-
-
-
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -30,9 +21,7 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule
   ],
